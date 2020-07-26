4071ad5b9e0a1e733db91a326f135ea01afa0acd
Luey-A <lueyahmad@gmail.com>
2020-07-26 17:57:52 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+main.remove()
+
+
+let newHeader = document.createElement('h1')
+newHeader.id = "victory"
+document.body.appendChild(newHeader)
+
+
+newHeader.innerHTML = 'L is the champion'
+
+
